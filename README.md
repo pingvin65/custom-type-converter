60407b6c193ea567bd53f62a4727cb8a482c5b8c
Sasa <budai8369@gmail.com>
2020-07-08 21:47:41 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# spring-custom-type-converter
---
+++ src/main/java/com/example/data/SpringCustomTypeConverterApplication.java
@@ -0,0 +1,13 @@
+package com.example.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCustomTypeConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCustomTypeConverterApplication.class, args);
+	}
+
+}
